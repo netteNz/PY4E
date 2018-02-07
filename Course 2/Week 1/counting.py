247e7b563f1e818462bf2485ed7179c99e57e878
Emanuel Lugo <33048912+netteNz@users.noreply.github.com>
2018-02-06 23:53:52 -0400

    Add files via upload

---
+++ Course 2/Week 1/counting.py
@@ -0,0 +1,6 @@
+word = 'banana'
+cnt =  0
+for letter in word:
+    if letter == 'a':
+        cnt = cnt + 1
+print(cnt)
---
+++ Course 2/Week 1/strings2.py
@@ -0,0 +1,6 @@
+fruit = 'banana'
+'n' in fruit
+'m' in fruit
+'nan' in fruit 
+if 'a' in fruit:
+    print('Found it')
